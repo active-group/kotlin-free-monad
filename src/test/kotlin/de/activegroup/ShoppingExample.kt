eab6166880959ecb8becc89dc7f1183396ee9e1f
Mike Sperber <sperber@deinprogramm.de>
2023-05-19 10:30:00 +0200

    Unbreak fork/join.

--- src/test/kotlin/de/activegroup/ShoppingExample.kt
+++ src/test/kotlin/de/activegroup/ShoppingExample.kt
@@ -16,13 +16,13 @@ sealed interface Shopping<out A> {
         override fun <B> bind(next: (A) -> Shopping<B>): Shopping<B> =
             GetCustomer(id) { customer -> cont(customer).bind(next) }
     }
-    data class Fork<R, out A>(val computation: Shopping<R>, val cont: (Shopping<R>) -> Shopping<A>): Shopping<A> {
+    data class Fork<R, out A>(val computation: Shopping<R>, val cont: (Future<R>) -> Shopping<A>): Shopping<A> {
         override fun <B> bind(next: (A) -> Shopping<B>): Shopping<B> =
             Fork(computation) { forked -> cont(forked).bind(next) }
     }
-    data class Join<out A>(val thunk: () -> Any): Shopping<A> {
+    data class Join<R, out A>(val future: Future<R>, val cont: (Any) -> Shopping<A>): Shopping<A> {
         override fun <B> bind(next: (A) -> Shopping<B>): Shopping<B> =
-            next(thunk() as A)
+            Join(future) { result -> cont(result).bind(next) }
     }
     data class Pure<out A>(val result: A): Shopping<A> {
         override fun <B> bind(next: (A) -> Shopping<B>): Shopping<B> = next(result)
@@ -41,37 +41,38 @@ class ShoppingDsl {
         Shopping.GetArticle(id) { Shopping.Pure(it) }.susp()
     suspend fun getCustomer(id: Int): Customer =
         Shopping.GetCustomer(id) { Shopping.Pure(it) }.susp()
-    suspend fun <R> fork(computation: Shopping<R>): Shopping<R> =
+    suspend fun <R> fork(computation: Shopping<R>): Future<R> =
         Shopping.Fork(computation) { Shopping.Pure(it)}.susp()
+    suspend fun <R> join(future: Future<R>): R =
+        Shopping.Join(future) { Shopping.Pure(it as R)}.susp()
     suspend fun <A> pure(result: A): A =
         MonadDSL.pure<Shopping<A>, A>(result) { Shopping.Pure(it) }
 }
 
-data class Future<out A>(val thunk: () -> A)
+data class Future<out A>(val thunk: () -> Any) {
+    fun force() = thunk() as A
+}
 
 val example =
     Shopping.Fork(Shopping.GetCustomer(1) { Shopping.Pure(it) }) { c1p ->
         Shopping.Fork(Shopping.GetArticle(1) { Shopping.Pure(it) }) { a1p ->
-            c1p.bind { c1 -> a1p.bind { a1 -> Shopping.Pure(c1.firstName + a1.name) }}
-    } }
+            Shopping.Join(c1p) { c1 -> Shopping.Join(a1p) { a1 -> Shopping.Pure((c1 as Customer).firstName + (a1 as Article).name) }}}}
 
 tailrec fun <A> shop(comp: Shopping<A>): A =
     when (comp) {
         is Shopping.GetArticle -> shop(comp.cont(Article(comp.id, "article")))
         is Shopping.Fork<*, A> ->
-            shop(comp.cont(Shopping.Join( { shop(comp.computation)!! })))
+            shop(comp.cont(Future({ shop(comp.computation)!! })))
         is Shopping.GetCustomer -> shop(comp.cont(Customer(comp.id.toLong(), "first", "last")))
-        is Shopping.Join -> comp.thunk() as A
+        is Shopping.Join<*, A> -> shop(comp.cont(comp.future.thunk()))
         is Shopping.Pure -> comp.result
     }
 
-// FIXME: bombs with already resumed at this point
-
 val exampleDsl = shopping {
-    val c1p = fork(shopping { getCustomer(1)} )
-    val a1p = fork(shopping { getArticle(1)} )
-    val c1 = c1p.susp()
-    val a1 = a1p.susp()
+    val c1p = fork(shopping { pure(getCustomer(1)) } )
+    val a1p = fork(shopping { pure(getArticle(1)) } )
+    val c1 = join(c1p)
+    val a1 = join(a1p)
     pure(c1.firstName + a1.name)
 }
 
